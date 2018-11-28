fbef70a66b8a25aaf562d8936b2d8e36199e6ccb
975261413@qq.com <Phone15810813104>
2018-11-28 22:56:16 +0800

    1.从控制台以及内存读取配置

---
+++ CommandLineSample/CommandLineSample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+namespace CommandLineSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dic = new Dictionary<string, string>()
+            {
+                {"name","Bean"},
+                {"age","25"}
+            };
+            var builder = new ConfigurationBuilder()
+                .AddInMemoryCollection(dic)//从内存读取
+                .AddCommandLine(args);//从控制台获取输入参数 or 通过控制台 dotnet run name=666 age=30
+
+            //*注意：配置会覆盖，比如上面的操作：控制台命令会覆盖内存配置
+
+            var configuration = builder.Build();
+
+            Console.WriteLine($"name:{configuration["name"]}");
+            Console.WriteLine($"age:{configuration["age"]}");
+
+            Console.ReadLine();
+        }
+    }
+}
